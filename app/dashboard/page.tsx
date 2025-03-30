d741f8fcb707bdce0199fb773dd2071c1d50e8ad
tanasatit <tanasatit.n@ku.th>
2025-03-30 15:36:35 +0700

    implement a revenue chart in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import  RevenueChart  from '@/app/ui/dashboard/revenue-chart'; 
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
+    const revenue = await fetchRevenue();
+    
     return (
         <main>
             <h1 className={'${lusitana.className} mb-4 text-xl md:text-2xl'}>
@@ -13,14 +16,10 @@ export default async function Page() {
                 {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
                 {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
                 {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-                {/* <Card
-                    title="Total Customers"
-                    value={numberOfCustomers}
-                    type="customers"
-            /> */}
+                {/* <Card title="Total Customers" value={numberOfCustomers} type="customers"/> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {/* <RevenueChart revenue={revenue}  /> */}
+                <RevenueChart revenue={revenue}  />
                 {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
             </div>
         </main>
