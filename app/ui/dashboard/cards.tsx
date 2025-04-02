8942fa1116626004ef14c5dc14fccd7f74c94893
tanasatit <tanasatit.n@ku.th>
2025-04-02 21:32:24 +0700

    fix some bug of UI, implement pre-render

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -25,9 +25,9 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>
       <Card
         title="Total Customers"
         value={numberOfCustomers}
