c03e61b44f8c4b20f4f29987dfc7750eee06251a
tanasatit <tanasatit.n@ku.th>
2025-04-09 03:28:48 +0700

    improve accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,9 +8,13 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0.'}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.'
+    }),
     date: z.string(),
 
 });
@@ -18,12 +22,32 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true});
 const UpdateInvoice = FormSchema.omit({ id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+export async function createInvoice(prevState: State, formData: FormData) {
+    // validate form using Zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        }
+    }
+
+    // prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -35,6 +59,9 @@ export async function createInvoice(formData: FormData) {
     } catch (error) {
         // We'll log the error to the console for now
         console.error(error);
+        return {
+            message: 'Database Error: Failed to create Invoice.',
+        };
     }
 
     revalidatePath('/dashboard/invoices');
@@ -42,33 +69,42 @@ export async function createInvoice(formData: FormData) {
     
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status} = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+  ) {
+    const validatedFields = UpdateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
     });
-    const amountInCents = amount * 100;
-    
-    try{
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId},
-        amount = ${amountInCents},
-        status = ${status}
-    WHERE id = ${id}
-    `;
-    } catch (error) {
-        // We'll log the error to the console for now
-        console.error(error);
+   
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
     }
-
+   
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+   
+    try {
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+   
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-}
+  }
 
 export async function deleteInvoice(id: string){
-    throw new Error('Failed to Delete Invoice');
     
     await sql`
     DELETE from invoices WHERE id = ${id}
